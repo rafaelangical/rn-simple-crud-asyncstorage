c55a0eeeac4d05660849ad57bcae01c1b439ca56
Rafael Souza <rafael.nascimento@softvaro.com.br>
2024-12-10 15:20:02 -0300

    chore: clean up

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,6 @@ import * as SplashScreen from "expo-splash-screen";
 import * as React from "react";
 import { Navigation } from "./navigation";
 
-Asset.loadAsync([
-  ...NavigationAssets,
-  require("./assets/newspaper.png"),
-  require("./assets/bell.png"),
-]);
-
 SplashScreen.preventAutoHideAsync();
 
 export function App() {
